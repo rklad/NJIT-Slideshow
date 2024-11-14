06bb5aa2d4554a72662839dd963e67d2f4936829
rklad <525RKladerman@frhsd.com>
2024-11-14 13:17:07 -0500

    added timer

--- gallery.js
+++ gallery.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
   $('.details').hide() // Hide details initially
 
   // Call a function here to start the timer for the slideshow
-
+  startTimer()
   // Select the moreIndicator button and add a click event to:
   // - toggle the rotation classes (rot90 and rot270)
   // - slideToggle the visibility of the .details section
@@ -84,6 +84,7 @@ function showPrevPhoto () {
 
 // Starter code for the timer function
 function startTimer () {
+  timer = setInterval(showNextPhoto, mWaitTime)
   // Create a timer to automatically call `showNextPhoto()` every mWaitTime milliseconds
   // Consider using setInterval to achieve this functionality
   // Hint: Make sure only one timer runs at a time
